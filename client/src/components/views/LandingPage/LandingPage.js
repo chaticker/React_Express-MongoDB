f51586707ef856601a193ec5b7d3a78ef541920f
chaticker <cseun999@naver.com>
2021-03-15 11:02:28 +0900

    antd css framework 추가

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -5,7 +5,8 @@ function LandingPage() {
 
     useEffect(() => {
         //get request를 서버에 보냄
-        axios.get('http://localhost:5000/api/hello')
+        axios.get('/api/hello')
+        //서버에서 돌아오는 response를 콘솔창에 출력
         .then(response => console.log(response.data))
     }, [])
 
